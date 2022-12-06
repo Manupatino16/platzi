5b23241aee0828e1c0b7a5c88c82bfc35e638402
Manuela Patiño Betancur <mpatinob@sura.com.co>
2022-12-06 16:39:44 -0500

    continuacion

--- Mokepon/js/mokepon.js
+++ Mokepon/js/mokepon.js
@@ -1,24 +1,106 @@
+let ataqueJugador
+let ataqueEnemigo
+
 function iniciarJuego(){
     let botonMascotaJugador = document.getElementById("boton-mascota")
     botonMascotaJugador.addEventListener("click",seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById("boton-fuego")
+    botonFuego.addEventListener("click", ataqueFuego)
+    let botonAgua = document.getElementById("boton-agua")
+    botonAgua.addEventListener("click", ataqueAgua)
+    let botonTierra = document.getElementById("boton-tierra")
+    botonTierra.addEventListener("click", ataqueTierra)
 }
 
 function seleccionarMascotaJugador(){
     let inputHipodoge = document.getElementById("hipodoge")
     let inputCapipepo = document.getElementById("capipepo")
     let inputRatigueya = document.getElementById("ratigueya")
+    let spanMascotaJugador = document.getElementById("mascota-jugador")
 
     if (inputHipodoge.checked){
-        alert("seleccionaste a Hipodoge")
+        spanMascotaJugador.innerHTML = "Hipodoge"
     }else if(inputCapipepo.checked){
-        alert("seleccionaste a Capipepo")
+        spanMascotaJugador.innerHTML = "Capipepo"
     }else if(inputRatigueya.checked){
-        alert("seleccionaste a Ratigueya")
+        spanMascotaJugador.innerHTML = "Ratigueya"
     }else{
         alert("Selecciona una mascota")
     }
+
+    seleccionarMascotaEnemigo()
+
 }
 
+function seleccionarMascotaEnemigo(){
+    let mascotaAleatoria = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+
+    if(mascotaAleatoria == 1){
+        spanMascotaEnemigo.innerHTML = "Hipodoge"
+    }else if (mascotaAleatoria == 2){
+        spanMascotaEnemigo.innerHTML = "Capipepo"
+    }else{
+        spanMascotaEnemigo.innerHTML = "Ratigueya"
+    }
+}
+
+function ataqueFuego(){
+    ataqueJugador = "FUEGO 🔥"
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAgua(){
+    ataqueJugador = "AGUA 💧"
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueTierra(){
+    ataqueJugador = "TIERRA 🍃"
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3)
+    if(ataqueAleatorio == 1){
+        ataqueEnemigo = "FUEGO 🔥"
+    }else if(ataqueAleatorio == 2){
+        ataqueEnemigo = "AGUA 💧"
+    }else{
+        ataqueEnemigo = "TIERRA 🍃"
+    }
+
+    combate()
+}
+
+function combate(){
+    if(ataqueEnemigo == ataqueJugador){
+        crearMensaje("EMPATE 🤜🏽🤛🏽")
+    }else if(ataqueJugador == "FUEGO 🔥" && ataqueEnemigo == "TIERRA 🍃"){
+        acrearMensajelert("GANASTE 👏🏽🎊")
+    }else if(ataqueJugador ==  "AGUA 💧" && ataqueEnemigo == "FUEGO 🔥"){
+        crearMensaje("GANASTE 👏🏽🎊")
+    }else if(ataqueJugador == "TIERRA 🍃" && ataqueEnemigo == "AGUA 💧"){
+        crearMensaje("GANASTE 👏🏽🎊")
+    }else{
+        crearMensaje("PERDISTE 😥")
+    }
+}
+
+
+function crearMensaje (resultado){
+    let sectionMensajes = document.getElementById("mensajes")
+
+    let parrafo = document.createElement("p")
+    parrafo.innerHTML = "Tu mascota ataco con " + ataqueJugador + " ,la mascota del enemigo ataco con " + ataqueEnemigo + " - " + resultado
+
+    sectionMensajes.appendChild(parrafo)
+}
+
+function aleatorio (min,max){
+    return Math.floor(Math.random() * (max - min +1) + min)
+}
 
 
 window.addEventListener("load", iniciarJuego)
